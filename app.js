b71eb84110bd6ad4eb0f3421e5a0c6a27404943b
Zayan Mustafa <mustafazayyan13@gmail.com>
2024-08-02 22:10:21 +0500

    Just Start

---
+++ app.js
@@ -0,0 +1,5 @@
+var userName = "Zayyan Mustafa";
+var userAge = 17;
+var imanParKhamtma = true;
+console.log("userName==>", userName);
+console.log("userAge==>", userAge);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let userName : string = "Zayyan Mustafa"
+let userAge : number = 17
+let imanParKhamtma : boolean = true
+
+console.log("userName==>" , userName)
+console.log("userAge==>" , userAge)
+
